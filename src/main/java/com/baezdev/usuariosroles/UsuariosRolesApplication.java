1b93ec73f6038334141f0d78e78121e671c77afe
baezdev <baezdev@gmail.com>
2024-09-11 14:21:18 -0600

    Setup SpringBoot project with MongoDB

---
+++ src/main/java/com/baezdev/usuariosroles/UsuariosRolesApplication.java
@@ -0,0 +1,13 @@
+package com.baezdev.usuariosroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosRolesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuariosRolesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baezdev/usuariosroles/UsuariosRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baezdev.usuariosroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosRolesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
